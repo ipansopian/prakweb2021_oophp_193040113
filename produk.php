082dc8d5281d023ed6ceee62682315261c7edc55
ipansopian <60495718+ipansopian@users.noreply.github.com>
2021-09-28 21:06:08 -0700

    Latihan OoPHP
    Latihan Pertemuan ke 1-3

---
+++ produk.php
@@ -0,0 +1,12 @@
+<?php
+
+class Produk {
+    public  $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
+}
+
+$produk1 = new Produk();
+$produk1->judul = "Naruto"
+$produk2 = new Produk();
